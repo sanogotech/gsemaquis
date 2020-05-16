806a3d0e7a3b985584edcc2c336f8579739d7b7f
ETIEN Ghislain Romuald <ghislain.etien@orange.com>
2020-05-16 11:37:32 +0000

    second commit

---
+++ src/main/java/com/mind2codes/gsemaquis/config/WebConfig.java
@@ -0,0 +1,146 @@
+package com.mind2codes.gsemaquis.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.Validator;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		/*registry.addMapping("/**")
+        .allowedOrigins("*")
+        .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");*/
+	}
+
+	@Override
+	public void addFormatters(FormatterRegistry arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// TODO Auto-generated method stub
+		registry.addResourceHandler("swagger-ui.html")
+        .addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**")
+        .addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+
+	@Override
+	public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configureAsyncSupport(AsyncSupportConfigurer arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void extendMessageConverters(List<HttpMessageConverter<?>> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void configurePathMatch(PathMatchConfigurer configurer) {
+	    // turn off all suffix pattern matching
+		configurer.setUseSuffixPatternMatch(false);
+		// OR
+		// turn on suffix pattern matching ONLY for suffixes
+		// you explicitly register using
+		// configureContentNegotiation(...)
+		    configurer.setUseRegisteredSuffixPatternMatch(false);
+	}
+
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+	    configurer.favorPathExtension(false);
+	}
+
+	@Override
+	public MessageCodesResolver getMessageCodesResolver() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Validator getValidator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
