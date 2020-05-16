2d3781803d8145acab9bff7dbcdfc3a24ebc56c6
ETIEN Ghislain Romuald <ghislain.etien@orange.com>
2020-05-16 08:33:59 +0000

    first commit

---
+++ src/main/java/com/mind2codes/gsemaquis/GsemaquisApplication.java
@@ -0,0 +1,13 @@
+package com.mind2codes.gsemaquis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsemaquisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsemaquisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mind2codes/gsemaquis/GsemaquisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mind2codes.gsemaquis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsemaquisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
